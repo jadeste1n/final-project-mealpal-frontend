b4b27bf32b61cf98df88b6602cc36b22c1ebcd9e
Lena Hauer <lena.hauer@me.com>
2025-04-17 20:41:46 +0200

    Fixed recipe imports

--- src/pages/recipes/SuggestedRecipe.jsx
+++ src/pages/recipes/SuggestedRecipe.jsx
@@ -1,50 +1,57 @@
-import React, {useEffect, useState} from 'react';
-import {useLocation} from 'react-router-dom';
-import { api } from 'utils/axiosInstance.jsx';
-import RecipeGrid from 'src/components/recipes/RecipeGrid.jsx';
-import RecipeIngredients from 'src/components/recipes/RecipeIngredient.jsx';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { api } from "@/utils/axiosInstance.jsx";
+import RecipeGrid from "@/components/recipes/RecipeGrid.jsx";
+import RecipeIngredients from "@/components/recipes/RecipeIngredient.jsx";
 
 const SuggestedRecipe = () => {
-    const locaton = useLocation();
-    const [recipes, setRecipes] = useState([]);
-    const [selectedRecipe, SetSelectedRecipe] = useState(null);
-    const [loading, setLoading] = useState (false);
+  const locaton = useLocation();
+  const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, SetSelectedRecipe] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    useEffect (()=> {
-        const fetchRecipes = async () => {
-            setLoading (true);
-            const params = new URLSearchParams(location.search);
-            const useFridge = params.get('fridge') === 'true';
-            const mealType= params.get ('mealType');
-            const maxReadyTime = params.get('maxReadyTime');
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      setLoading(true);
+      const params = new URLSearchParams(location.search);
+      const useFridge = params.get("fridge") === "true";
+      const mealType = params.get("mealType");
+      const maxReadyTime = params.get("maxReadyTime");
 
-            try {
-                const body = useFridge ? {ingredients: ['chicken', 'broccoli'],refresh:true} // TODo will replace this with actual fridge items
-                :{mealType, maxReadyTime: Number(maxReadyTime),refresh:true};
+      try {
+        const body = useFridge
+          ? { ingredients: ["chicken", "broccoli"], refresh: true } // TODo will replace this with actual fridge items
+          : { mealType, maxReadyTime: Number(maxReadyTime), refresh: true };
 
-                const res = await api.post ('/recipes/suggest', body);
-                setRecipes(res.data);
+        const res = await api.post("/recipes/suggest", body);
+        setRecipes(res.data);
+      } catch (err) {
+        console.error("Error fetching recipes", err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-            } catch (err) {
-                console.error('Error fetching recipes', err);
-            } finally{
-                setLoading(false);
-            }
-        };
+    fetchRecipes();
+  }, [locaton.search]);
 
-        fetchRecipes();
-    },[locaton.search]);
+  return (
+    <div className="p-4">
+      <h1 className="text-x1 font-semibold mb-4">Suggested Recipes</h1>
+      {loading ? (
+        <p> Loading Recipes...</p>
+      ) : (
+        <RecipeGrid recipes={recipes} onselect={SetSelectedRecipe} />
+      )}
 
-    return (
-        <div className='p-4'>
-            <h1 className='text-x1 font-semibold mb-4'>Suggested Recipes</h1>
-            {loading ? (
-                <p> Loading Recipes...</p>
-            ): ( <RecipeGrid recipes = {recipes} onselect= {SetSelectedRecipe}/>)}
-
-            {selectedRecipe && (<RecipeIngredients recipe ={selectedRecipe} onClose = {()=> SetSelectedRecipe(null)} />)}
-        </div>
-    );
+      {selectedRecipe && (
+        <RecipeIngredients
+          recipe={selectedRecipe}
+          onClose={() => SetSelectedRecipe(null)}
+        />
+      )}
+    </div>
+  );
 };
 
 export default SuggestedRecipe;
