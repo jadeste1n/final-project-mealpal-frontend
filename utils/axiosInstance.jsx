bde317a5fbfafc558420966c5ce0086b989b28a4
hlenaa <80155550+hlenaa@users.noreply.github.com>
2025-04-17 20:43:47 +0200

    Delete utils directory

--- utils/axiosInstance.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL: 'https://mealpal-backend.onrender.com',
-    headers: {
-        'Conten-Type': 'application/json'
-    }
-});
-
