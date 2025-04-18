82d64715952b8a446d6e281b68b2e8a59d0f5e10
Jackie <jacquelinederigs@gmail.com>
2025-04-18 16:11:50 +0200

    updated MainLayout

--- src/components/general/NavBar.jsx
+++ src/components/general/NavBar.jsx
@@ -1,36 +1,29 @@
 import { Link } from "react-router-dom";
-import { Refrigerator, BookOpenCheck, Soup, User, Plus } from "lucide-react";
+import LogoHorizontal from "/src/assets/logo-horizontal.svg?react";
 
 const NavBar = () => {
-  return (
-    <div className="fixed bottom-0 left-0 right-0 z-50">
-      <div className="dock dock-xs bg-gray-300 relative">
-        {/* Floating Action Button inside the dock */}
-        <div className="absolute -top-6 left-1/2 transform -translate-x-1/2 z-10">
-          <Link to="/search">
-            <button className="btn btn-lg btn-circle bg-green-400 text-white hover:bg-green-500 shadow-lg">
-              <Plus className="w-8 h-8" />
-            </button>
-          </Link>
-        </div>
+	const color1 = "#154836";
+	const color2 = "#154836 ";
+	const color3 = "#154836";
+	const color4 = "#E06138 ";
+	const color5 = "#154836";
 
-        {/* Dock Items */}
-        <Link to="/" className="btn-ghost btn-sm rounded-btn">
-          <Refrigerator className="w-5 h-5" />
-        </Link>
-        <Link to="/diary" className="btn-ghost rounded-btn">
-          <BookOpenCheck className="w-5 h-5" />
-        </Link>
-        <div className="w-12" />
-        <Link to="/recipes" className="btn-ghost rounded-btn">
-          <Soup className="w-5 h-5" />
-        </Link>
-        <Link to="/account" className="btn-ghost rounded-btn">
-          <User className="w-5 h-5" />
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="navbar bg-base-200 shadow-sm mb-6 sticky top-0 z-50 flex justify-left">
+			<a className="text-xl p-6 bg-base-200 rounded-md h-10 flex items-center">
+				<LogoHorizontal
+					className="w-24"
+					style={{
+						"--color1": color1,
+						"--color2": color2,
+						"--color3": color3,
+						"--color4": color4,
+						"--color5": color5,
+					}}
+				/>
+			</a>
+		</div>
+	);
 };
 
 export default NavBar;
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
-import { defineConfig } from 'vite'
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  plugins: [react(), tailwindcss()],
-})
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
+	plugins: [
+		react(),
+		tailwindcss(),
+		svgr({
+			exportAsDefault: false, // <-- allows named export (ReactComponent)
+			svgrOptions: {
+				icon: true,
+			},
+		}),
+	],
+});
